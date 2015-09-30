c3ac856bc63356518d30ddef3b44469cb8081d9d
samjarzembowski <sjarzembowski@gmail.com>
2015-09-30 12:05:47 -0700

    initial commit

---
+++ game.js
@@ -0,0 +1,13 @@
+var question1 = prompt('Was I born in Massachusetts?');
+var question2 = prompt('Did I grow up in Illinois?');
+var question3 = prompt('Do I volunteer at Woodland Park Zoo?');
+
+console.log('question1 response: ' + question1);
+console.log('question2 response: ' + question2);
+console.log('question3 response: ' + question3);
+
+alert('Your answer to "Was I born in Massachusetts" was ' + question1);
+alert('Your answer to "Did I grow up in Illinois" was ' + question2);
+alert('Your answer to "Do I volunteer at Woodland Park Zoo" was ' + question3);
+
+
