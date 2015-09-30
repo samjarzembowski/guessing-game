bb299c444a3e9cbc82e713eebec6f7a9ad331097
samjarzembowski <sjarzembowski@gmail.com>
2015-09-30 14:37:09 -0700

    Added a thank you message/summary and a variable to track correct answers

--- game.js
+++ game.js
@@ -1,3 +1,4 @@
+var correctAnswers = 0;
 var user = prompt('Please enter your name:');
 alert('Thank you, ' + user + '.  Press ok to play the Guessing Game!');
 
@@ -5,25 +6,33 @@ var question1 = prompt('Was I born in Massachusetts?');
 var question1 = question1.toLowerCase();
 
 if (question1 === 'yes' || question1 === 'y') {
-  alert('Correct!  I was born in Massachusetts.');
+  alert('Correct ' + user +'!  I was born in Massachusetts.');
+  correctAnswers++;
 } else {
-  alert('Sorry!  I was actually born in Massachusetts.');
+  alert('Sorry ' + user +'!  I was actually born in Massachusetts.');
 }
+
 
 var question2 = prompt('Did I grow up in Illinois?');
 var question2 = question2.toLowerCase();
 
 if (question2 === 'yes' || question2 === 'y') {
-  alert('Correct!  I did grow up in Illinois.');
+  alert('Correct ' + user +'!  I did grow up in Illinois.');
+  correctAnswers++;
 } else {
-  alert('Sorry!  I actually did grow up in Illinois.');
+  alert('Sorry ' + user +'!  I actually did grow up in Illinois.');
 }
+
 
 var question3 = prompt('Do I volunteer at Woodland Park Zoo?');
 var question3 = question3.toLowerCase();
 
 if (question3 === 'yes' || question3 === 'y') {
-  alert('Correct!  I do volunteer at Woodland Park Zoo.');
+  alert('Correct ' + user +'!  I do volunteer at Woodland Park Zoo.');
+  correctAnswers++
 } else {
-  alert('Sorry!  I actually do volunteer at Woodland Park Zoo.');
+  alert('Sorry ' + user +'!  I actually do volunteer at Woodland Park Zoo.');
 }
+
+
+alert('Thanks for playing, ' + user + '.  You had ' + correctAnswers + ' out of 3 correct answers!');
