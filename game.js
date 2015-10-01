627175be5485c50258d7478f0b09cf9fd18e5c76
Austin Sprague <Sprague@Austins-MacBook-Pro.local>
2015-10-01 13:32:33 -0700

    Added functions and ElementId

--- game.js
+++ game.js
@@ -1,38 +1,49 @@
+var one = document.getElementById('one');
+var two = document.getElementById('two');
+var three = document.getElementById('three');
+
 var correctAnswers = 0;
 var user = prompt('Please enter your name:');
 alert('Thank you, ' + user + '.  Press ok to play the Guessing Game!');
 
-var question1 = prompt('Was I born in Massachusetts?');
-var question1 = question1.toLowerCase();
+function ques1(){
+	var question1 = prompt('Was I born in Massachusetts?');
+	var question1 = question1.toLowerCase();
 
-if (question1 === 'yes' || question1 === 'y') {
-  alert('Correct ' + user +'!  I was born in Massachusetts.');
-  correctAnswers++;
-} else {
-  alert('Sorry ' + user +'!  I was actually born in Massachusetts.');
+	if (question1 === 'yes' || question1 === 'y') {
+		alert('Correct ' + user +'!  I was born in Massachusetts.');
+		correctAnswers++;
+		} else {
+		alert('Sorry ' + user +'!  I was actually born in Massachusetts.');
+		}
 }
 
+function ques2(){
+	var question2 = prompt('Did I grow up in Illinois?');
+	var question2 = question2.toLowerCase();
 
-var question2 = prompt('Did I grow up in Illinois?');
-var question2 = question2.toLowerCase();
-
-if (question2 === 'yes' || question2 === 'y') {
-  alert('Correct ' + user +'!  I did grow up in Illinois.');
-  correctAnswers++;
-} else {
-  alert('Sorry ' + user +'!  I actually did grow up in Illinois.');
+	if (question2 === 'yes' || question2 === 'y') {
+		alert('Correct ' + user +'!  I did grow up in Illinois.');
+		correctAnswers++;
+		} else {
+		alert('Sorry ' + user +'!  I actually did grow up in Illinois.');
+		}
 }
 
+function ques3(){
+	var question3 = prompt('Do I volunteer at Woodland Park Zoo?');
+	var question3 = question3.toLowerCase();
 
-var question3 = prompt('Do I volunteer at Woodland Park Zoo?');
-var question3 = question3.toLowerCase();
-
-if (question3 === 'yes' || question3 === 'y') {
-  alert('Correct ' + user +'!  I do volunteer at Woodland Park Zoo.');
-  correctAnswers++
-} else {
-  alert('Sorry ' + user +'!  I actually do volunteer at Woodland Park Zoo.');
+	if (question3 === 'yes' || question3 === 'y') {
+		alert('Correct ' + user +'!  I do volunteer at Woodland Park Zoo.');
+		correctAnswers++
+		} else {
+		alert('Sorry ' + user +'!  I actually do volunteer at Woodland Park Zoo.');
+		}
 }
 
+ques1();
+ques2();
+ques3();
 
 alert('Thanks for playing, ' + user + '.  You had ' + correctAnswers + ' out of 3 correct answers!');
