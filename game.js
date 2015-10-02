71135d0ee62e7d7af9b239c8792cf17181ad2f57
Roland D. San Nicolas <rdsannicolas@gmail.com>
2015-10-02 12:59:26 -0700

    initial push

--- game.js
+++ game.js
@@ -1,16 +1,18 @@
 var one = document.getElementById('one');
 var two = document.getElementById('two');
 var three = document.getElementById('three');
+var questions = ['Was I born in Massachusetts?', 'Did I grow up in Illinois?', 'Do I volunteer at Woodland Park Zoo?'];
+var answers = ["yes", "y"];
 
 var correctAnswers = 0;
 var user = prompt('Please enter your name:');
 alert('Thank you, ' + user + '.  Press ok to play the Guessing Game!');
 
 function ques1(){
-	var question1 = prompt('Was I born in Massachusetts?');
+	var question1 = prompt(questions[0]);
 	var question1 = question1.toLowerCase();
 
-	if (question1 === 'yes' || question1 === 'y') {
+	if (question1 === answers[0] || question1 === answers[1]) {
 		one.innerHTML = 'Correct ' + user +'!  I was born in Massachusetts.';
 		correctAnswers++;
 		} else {
@@ -18,11 +20,12 @@ function ques1(){
 		}
 }
 
+
 function ques2(){
-	var question2 = prompt('Did I grow up in Illinois?');
+	var question2 = prompt(questions[1]);
 	var question2 = question2.toLowerCase();
 
-	if (question2 === 'yes' || question2 === 'y') {
+	if (question2 === answers[0] || question2 === answers[1]) {
 		two.innerHTML = 'Correct ' + user +'!  I did grow up in Illinois.';
 		correctAnswers++;
 		} else {
@@ -31,10 +34,10 @@ function ques2(){
 }
 
 function ques3(){
-	var question3 = prompt('Do I volunteer at Woodland Park Zoo?');
+	var question3 = prompt(questions[2]);
 	var question3 = question3.toLowerCase();
 
-	if (question3 === 'yes' || question3 === 'y') {
+	if (question3 === answers[0] || question3 === answers[1]) {
 		three.innerHTML = 'Correct ' + user +'!  I do volunteer at Woodland Park Zoo.';
 		correctAnswers++;
 		} else {
@@ -47,3 +50,4 @@ ques2();
 ques3();
 
 alert('Thanks for playing, ' + user + '.  You had ' + correctAnswers + ' out of 3 correct answers!');
+
